6f05df940f9f8271f7f1c2ce0887d870af94f566
Rajesh Shashi Kumar <35628747+rajesh-s@users.noreply.github.com>
2020-12-21 07:07:08 -0800

    Day 1: Completed

---
+++ day1/lab_day1/CAWS-LAB/ChampSim/tracer/clean_tracer.sh
@@ -0,0 +1,2 @@
+export PIN_ROOT=/home/rajesh/work/caws2020/selection_task/ChampSim-CAWS/tracer/PIN/pin-3.17-98314-g0c048d619-gcc-linux
+make clean
---
+++ day1/lab_day1/CAWS-LAB/ChampSim/tracer/make_tracer.sh
@@ -0,0 +1,3 @@
+export PIN_ROOT=/home/rajesh/work/caws2020/selection_task/ChampSim-CAWS/tracer/PIN/pin-3.17-98314-g0c048d619-gcc-linux
+mkdir -p obj-intel64
+make obj-intel64/champsim_tracer.so
